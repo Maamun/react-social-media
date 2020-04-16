00d7676b47aa8a88692bcaa803e2873652c6a5c3
Maamun <mamun.mokni@gmail.com>
2020-04-17 02:23:27 +0400

    useEffect basics

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
@@ -6,7 +6,14 @@ function App() {
     language: "javascript",
     experience: 0,
     employed: true,
+    name: "",
   });
+  useEffect(() => {
+    document.title = developer.name;
+    console.log("runs");
+  }, [developer.name]);
+  //to conditionaly run useEffect we need to know what piece of state we want to sync with, only run the useEffect callback when the name piece os state changes, we need to always need to be askimg question: what piece of state do we want our side effect tp sync with, we need to include it in the dependency array.
+
   function handleEmploymentStatus() {
     setDeveloper((prevState) => ({
       ...prevState,
@@ -15,6 +22,7 @@ function App() {
   }
   function handleLanguageChange() {
     setDeveloper({
+      ...developer,
       language: "ReasonML",
       experience: 0,
       isEmployed: false,
@@ -26,11 +34,23 @@ function App() {
       experience: event.target.value,
     });
   }
+  function handleTitleChange(e) {
+    setDeveloper({
+      ...developer,
+      name: e.target.value,
+    });
+  }
+
   return (
     <div>
       <button onClick={handleEmploymentStatus}>Toggle Employment Status</button>
       <button onClick={handleLanguageChange}>Change Language</button>
       <input type="number" min="0" onChange={handleInputChange} />
+      <input
+        type="text"
+        placeholder="change title"
+        onChange={handleTitleChange}
+      />
       <p>I am learning {developer.language}</p>
       <p>I have {developer.experience} years of experience.</p>
       <p>
