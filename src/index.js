38dcda3c490053245681e0066558ccca2c2ce69b
Maamun <mamun.mokni@gmail.com>
2020-04-17 00:54:20 +0400

    onClick & map list

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Person(props) {
+  function handlePersonClick(event) {
+    alert(props.person);
+    console.log("event", event);
+  }
+  return <li onClick={handlePersonClick}>{props.person}</li>;
+}
+function App() {
+  const list = ["souad", "nosnos", "yesso"];
+  function handleInputChange(event) {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+  }
+  return (
+    <ul>
+      {list.map((person, i) => (
+        <Person key={i} person={person} />
+      ))}
+      <input onClick={handleInputChange} />
+    </ul>
+  );
+}
 const rootNode = document.querySelector("#root");
 
-function Header(props) {
-  return <h1>hello {props.username}</h1>;
-}
-
-// one rule we must follow is that we can't mutate or directly change props.
-
-function Layout(props) {
-  return <div style={{ background: "palegoldenrod" }}>{props.children}</div>;
-}
-const isAuth = true;
-function Login() {
-  return <p>Please login!!</p>;
-}
-function SignOut() {
-  return <button>sign out</button>;
-}
-ReactDOM.render(
-  <Layout>
-    {isAuth ? (
-      <>
-        <Header username="nosnos" />
-        <SignOut />
-      </>
-    ) : (
-      <Login />
-    )}
-    {/*isAuth && <SignOut /> */}
-    <footer>copyright 2020</footer>
-  </Layout>,
-  rootNode
-);
+ReactDOM.render(<App />, rootNode);
