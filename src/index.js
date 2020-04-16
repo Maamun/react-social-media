280a58851a3a99d077a0ea24bc1cb82048445dc8
Maamun <mamun.mokni@gmail.com>
2020-04-17 00:18:21 +0400

    React props & children

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const rootNode = document.querySelector("#root");
-const year = 2020;
-const greeting2 = (
-  <div>
-    <h1>Hi yesso</h1>
-    <p>welcome from Sousse</p>
-  </div>
-);
-const isReactUser = true;
-const greeting = <h1>Hello react in {year}</h1>;
-if (isReactUser) {
-  ReactDOM.render(greeting2, rootNode);
-} else {
-  ReactDOM.render(<h1>Hello JavaScript </h1>, rootNode);
+
+function Header(props) {
+  return <h1>hello {props.username}</h1>;
 }
+
+// one rule we must follow is that we can't mutate or directly change props.
+
+function Layout(props) {
+  return <div style={{ background: "palegoldenrod" }}>{props.children}</div>;
+}
+ReactDOM.render(
+  <Layout>
+    <Header username="nosnos" />
+    <footer>copyright 2020</footer>
+  </Layout>,
+  rootNode
+);
