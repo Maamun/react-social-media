21f1be7b907268c6f6fdfe24bd9129449b3aa30a
Maamun <mamun.mokni@gmail.com>
2020-04-17 17:03:18 +0400

    useEffect dataFetch basics

--- src/index.js
+++ src/index.js
@@ -1,90 +1,35 @@
 import React, { Component, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-class IntervalClassCounter extends Component {
-  state = {
-    count: 0,
-  };
-  tick = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.tick();
-    }, 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return <div>{this.state.count}</div>;
-  }
-}
-
-function IntervalHookCounter() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const tick = () => {
-      setCount(count + 1);
-      //if we want an empty dependency array
-      // setCount((prevCount) => prevCount + 1);
-    };
-    console.log("use Effect called");
-    const interval = setInterval(() => {
-      tick();
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [count]);
-  return <div>{count}</div>;
-}
+const BASE_URL = `https://jsonplaceholder.typicode.com/posts/`;
 
 function App() {
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+  const [idFromButton, setIdFromButton] = useState(1);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}${idFromButton}`)
+      .then((res) => res.json())
+      .then((post) => setPost(post));
+  }, [id, idFromButton]);
+  function handleIdChange(e) {
+    setId(e.target.value);
+  }
+  function handleFetchPost() {
+    setIdFromButton(id);
+  }
   return (
     <div>
-      <IntervalClassCounter />
-      <IntervalHookCounter />
+      <input type="text" value={id} onChange={handleIdChange} />
+      <button onClick={handleFetchPost}>Fetch Post</button>
+      <p>
+        {post.id}: {post.title}
+      </p>
     </div>
   );
 }
 
-function App2() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  function handleMousePosition(e) {
-    console.log("Mouse event");
-    setMousePosition({
-      x: e.clientX,
-      y: e.clientY,
-    });
-  }
-  useEffect(() => {
-    console.log("useEffect called !");
-    window.addEventListener("mousemove", handleMousePosition);
-    return () => {
-      console.log("component Unmounting code");
-      window.removeEventListener("mousemove", handleMousePosition);
-    };
-  }, []);
-
-  return (
-    <p>
-      X: {mousePosition.x}, Y:{mousePosition.y}
-    </p>
-  );
-}
 const rootNode = document.querySelector("#root");
 
-function NewPage() {
-  return <div>hi from new page</div>;
-}
 ReactDOM.render(<App />, rootNode);
-
-// setTimeout(() => {
-//   ReactDOM.render(<NewPage />, rootNode);
-// }, 2000);
