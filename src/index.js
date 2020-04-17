1b821cc0fc0ee8e5334eab8fc37ace8f7cba2d65
Maamun <mamun.mokni@gmail.com>
2020-04-17 16:27:39 +0400

    useEffect cleanUp basics

--- src/index.js
+++ src/index.js
@@ -1,64 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-  const [developer, setDeveloper] = useState({
-    language: "javascript",
-    experience: 0,
-    employed: true,
-    name: "",
-  });
+class IntervalClassCounter extends Component {
+  state = {
+    count: 0,
+  };
+  tick = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <div>{this.state.count}</div>;
+  }
+}
+
+function IntervalHookCounter() {
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
-    document.title = developer.name;
-    console.log("runs");
-  }, [developer.name]);
-  //to conditionaly run useEffect we need to know what piece of state we want to sync with, only run the useEffect callback when the name piece os state changes, we need to always need to be askimg question: what piece of state do we want our side effect tp sync with, we need to include it in the dependency array.
+    const tick = () => {
+      setCount(count + 1);
+      //if we want an empty dependency array
+      // setCount((prevCount) => prevCount + 1);
+    };
+    console.log("use Effect called");
+    const interval = setInterval(() => {
+      tick();
+    }, 1000);
 
-  function handleEmploymentStatus() {
-    setDeveloper((prevState) => ({
-      ...prevState,
-      isEmployed: !prevState.isEmployed,
-    }));
-  }
-  function handleLanguageChange() {
-    setDeveloper({
-      ...developer,
-      language: "ReasonML",
-      experience: 0,
-      isEmployed: false,
-    });
-  }
-  function handleInputChange(event) {
-    setDeveloper({
-      ...developer,
-      experience: event.target.value,
-    });
-  }
-  function handleTitleChange(e) {
-    setDeveloper({
-      ...developer,
-      name: e.target.value,
-    });
-  }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
+  return <div>{count}</div>;
+}
 
+function App() {
   return (
     <div>
-      <button onClick={handleEmploymentStatus}>Toggle Employment Status</button>
-      <button onClick={handleLanguageChange}>Change Language</button>
-      <input type="number" min="0" onChange={handleInputChange} />
-      <input
-        type="text"
-        placeholder="change title"
-        onChange={handleTitleChange}
-      />
-      <p>I am learning {developer.language}</p>
-      <p>I have {developer.experience} years of experience.</p>
-      <p>
-        Employment status: {developer.isEmployed ? "Employed" : "Unemployed"}
-      </p>
+      <IntervalClassCounter />
+      <IntervalHookCounter />
     </div>
+  );
+}
+
+function App2() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  function handleMousePosition(e) {
+    console.log("Mouse event");
+    setMousePosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  }
+  useEffect(() => {
+    console.log("useEffect called !");
+    window.addEventListener("mousemove", handleMousePosition);
+    return () => {
+      console.log("component Unmounting code");
+      window.removeEventListener("mousemove", handleMousePosition);
+    };
+  }, []);
+
+  return (
+    <p>
+      X: {mousePosition.x}, Y:{mousePosition.y}
+    </p>
   );
 }
 const rootNode = document.querySelector("#root");
 
+function NewPage() {
+  return <div>hi from new page</div>;
+}
 ReactDOM.render(<App />, rootNode);
+
+// setTimeout(() => {
+//   ReactDOM.render(<NewPage />, rootNode);
+// }, 2000);
